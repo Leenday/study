7cb0ff6499349543ead946281c6ed6d77bd35709
Leenday <iammilagre@gmail.com>
2020-09-23 22:04:57 +0300

    solution of functional primitives 2

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,42 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        iter = lambda do |arr, index|
+          return arr if index == length
+
+          yield(arr[index]) if block_given?
+          iter.call(arr, index + 1)
+        end
+        iter.call(self, 0)
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        acc = Exercise::Fp2::MyArray.new
+        my_each do |element|
+          acc << yield(element)
+        end
+        acc
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        acc = Exercise::Fp2::MyArray.new
+        my_each do |element|
+          acc << element unless element.nil?
+        end
+        acc
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(acc = nil)
+        my_each do |element|
+          acc = acc.nil? ? element : yield(acc, element)
+          yield(acc, element) if block_given?
+        end
+        acc
+      end
     end
   end
 end
