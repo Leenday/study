b25aaeaba1de55ea9899a6c9eb985ef3a4d42d6c
Leenday <iammilagre@gmail.com>
2020-10-30 10:14:49 +0300

    omit lambdas and calls

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -6,27 +6,27 @@ module Exercise
       end
 
       def replace(array)
-        highest_num = find_max_num(array)
-        array.reduce([]) { |acc, num| acc << (num.positive? ? highest_num : num) }
+        max_num = find_max_num(array)
+        array.reduce([]) { |acc, num| acc << (num.positive? ? max_num : num) }
+      end
+
+      def iter(arr = [], query_num, indxs)
+        return -1 if indxs.size == 1 && arr[indxs.first] != query_num
+
+        mid_index = indxs.size / 2
+        mid_num = arr[indxs[mid_index]]
+        return indxs[mid_index] if mid_num == query_num
+
+        left, right = indxs.each_slice((indxs.size / 2.0).round).to_a
+        return iter(arr, query_num, left) if query_num <= mid_num
+        return iter(arr, query_num, right) if query_num >= mid_num
       end
 
       def search(array, query)
         raise ArgumentError, 'Argument is not numeric' unless query.is_a? Numeric
         return -1 if array.size <= 1 && array.first != query
-
-        iter = lambda do |arr = [], query_num, indxs|
-          return -1 if indxs.size == 1 && arr[indxs.first] != query_num
-
-          mid_index = indxs.size / 2
-          mid_num = arr[indxs[mid_index]]
-          return indxs[mid_index] if mid_num == query_num
-
-          left, right = indxs.each_slice((indxs.size / 2.0).round).to_a
-          return iter.call(arr, query_num, left) if query_num <= mid_num
-          return iter.call(arr, query_num, right) if query_num >= mid_num
-        end
         indexes = Array(0..array.length - 1)
-        iter.call(array, query, indexes)
+        iter(array, query, indexes)
       end
     end
   end
